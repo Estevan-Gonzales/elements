938d8bd75c6b9079bfd55ef35a2f661d004296ea
Estevan <egonz94@gmail.com>
2024-02-23 18:27:18 -0600

    Added data

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,11 @@
 function Table() {
     return (
         <div className="grid">
-            <div className="block reactive-nonmetals" id="hydrogen">H</div>
+            <div className="block reactive-nonmetals">
+                <div className="element-number">1</div>
+                <div className="element-symbol">H</div>
+                <div className="element-name">Hydrogen</div>
+            </div>
             <div className="block-empty"></div>
             <div className="block-empty"></div>
             <div className="block-empty"></div>
@@ -19,10 +23,73 @@ function Table() {
             <div className="block-empty"></div>
             <div className="block-empty"></div>
             <div className="block-empty"></div>
-            <div className="block noble-gases" id="helium">He</div>
+            <div className="block noble-gases">
+                <div className="element-number">2</div>
+                <div className="element-symbol">He</div>
+                <div className="element-name">Helium</div>
+            </div>
+            <div className="block alkali-metals">
+                <div className="element-number">3</div>
+                <div className="element-symbol">Li</div>
+                <div className="element-name">Lithium</div>
+            </div>
+            <div className="block alkaline-earth-metals">
+                <div className="element-number">4</div>
+                <div className="element-symbol">Be</div>
+                <div className="element-name">Beryllium</div>
+            </div>
+            <div className="block-empty"></div>
+            <div className="block-empty"></div>
+            <div className="block-empty"></div>
+            <div className="block-empty"></div>
+            <div className="block-empty"></div>
+            <div className="block-empty"></div>
+            <div className="block-empty"></div>
+            <div className="block-empty"></div>
+            <div className="block-empty"></div>
+            <div className="block-empty"></div>
+            <div className="block-empty"></div>
+            <div className="block metalloids">
+                <div className="element-number">5</div>
+                <div className="element-symbol">B</div>
+                <div className="element-name">Boron</div>
+            </div>
+            <div className="block reactive-nonmetals">
+                <div className="element-number">6</div>
+                <div className="element-symbol">C</div>
+                <div className="element-name">Carbon</div>
+            </div>
+            <div className="block reactive-nonmetals">
+                <div className="element-number">7</div>
+                <div className="element-symbol">N</div>
+                <div className="element-name">Nitrogen</div>
+            </div>
+            <div className="block reactive-nonmetals">
+                <div className="element-number">8</div>
+                <div className="element-symbol">O</div>
+                <div className="element-name">Oxygen</div>
+            </div>
+            <div className="block reactive-nonmetals">
+                <div className="element-number">9</div>
+                <div className="element-symbol">F</div>
+                <div className="element-name">Fluorine</div>
+            </div>
+            <div className="block noble-gases">
+                <div className="element-number">10</div>
+                <div className="element-symbol">Ne</div>
+                <div className="element-name">Neon</div>
+            </div>
 
-            <div className="block alkali-metals">Li</div>
-            <div className="block alkaline-earth-metals">Be</div>
+            <div className="block alkali-metals">
+                <div className="element-number">11</div>
+                <div className="element-symbol">Na</div>
+                <div className="element-name">Sodium</div>
+            </div>
+            <div className="block alkaline-earth-metals">
+                <div className="element-number">12</div>
+                <div className="element-symbol">Mg</div>
+                <div className="element-name">Magnesium</div>
+            </div>
             <div className="block-empty"></div>
             <div className="block-empty"></div>
             <div className="block-empty"></div>
@@ -34,56 +101,144 @@ function Table() {
             <div className="block-empty"></div>
             <div className="block-empty"></div>
             <div className="block-empty"></div>
-            <div className="block metalloids">B</div>
-            <div className="block reactive-nonmetals">C</div>
-            <div className="block reactive-nonmetals">N</div>
-            <div className="block reactive-nonmetals">O</div>
-            <div className="block reactive-nonmetals">F</div>
-            <div className="block noble-gases">Ne</div>
+            <div className="block post-transition-metals">
+                <div className="element-number">13</div>
+                <div className="element-symbol">Al</div>
+                <div className="element-name">Aluminum</div>
+            </div>
+            <div className="block metalloids">
+                <div className="element-number">14</div>
+                <div className="element-symbol">Si</div>
+                <div className="element-name">Silicon</div>
+            </div>
+            <div className="block reactive-nonmetals">
+                <div className="element-number">15</div>
+                <div className="element-symbol">P</div>
+                <div className="element-name">Phosphorus</div>
+            </div>
+            <div className="block reactive-nonmetals">
+                <div className="element-number">16</div>
+                <div className="element-symbol">S</div>
+                <div className="element-name">Sulfur</div>
+            </div>
+            <div className="block reactive-nonmetals">
+                <div className="element-number">17</div>
+                <div className="element-symbol">Cl</div>
+                <div className="element-name">Chlorine</div>
+            </div>
+            <div className="block noble-gases">
+                <div className="element-number">18</div>
+                <div className="element-symbol">Ar</div>
+                <div className="element-name">Argon</div>
+            </div>
 
-            <div className="block alkali-metals">Na</div>
-            <div className="block alkaline-earth-metals">Mg</div>
+            <div className="block alkali-metals">
+                <div className="element-number">19</div>
+                <div className="element-symbol">K</div>
+                <div className="element-name">Potassium</div>
+            </div>
+            <div className="block alkaline-earth-metals">
+                <div className="element-number">20</div>
+                <div className="element-symbol">Ca</div>
+                <div className="element-name">Calcium</div>
+            </div>
+            <div className="block transition-metals">
+                <div className="element-number">21</div>
+                <div className="element-symbol">Sc</div>
+                <div className="element-name">Scandium</div>
+            </div>
             <div className="block-empty"></div>
-            <div className="block-empty"></div>
-            <div className="block-empty"></div>
-            <div className="block-empty"></div>
-            <div className="block-empty"></div>
-            <div className="block-empty"></div>
-            <div className="block-empty"></div>
-            <div className="block-empty"></div>
-            <div className="block-empty"></div>
-            <div className="block-empty"></div>
-            <div className="block-empty"></div>
-            <div className="block post-transition-metals">Al</div>
-            <div className="block metalloids">Si</div>
-            <div className="block reactive-nonmetals">P</div>
-            <div className="block reactive-nonmetals">S</div>
-            <div className="block reactive-nonmetals">Cl</div>
-            <div className="block noble-gases">Ar</div>
+            <div className="block transition-metals">
+                <div className="element-number">22</div>
+                <div className="element-symbol">Ti</div>
+                <div className="element-name">Titanium</div>
+            </div>
+            <div className="block transition-metals">
+                <div className="element-number">23</div>
+                <div className="element-symbol">V</div>
+                <div className="element-name">Vanadium</div>
+            </div>
+            <div className="block transition-metals">
+                <div className="element-number">24</div>
+                <div className="element-symbol">Cr</div>
+                <div className="element-name">Chromium</div>
+            </div>
+            <div className="block transition-metals">
+                <div className="element-number">25</div>
+                <div className="element-symbol">Mn</div>
+                <div className="element-name">Manganese</div>
+            </div>
+            <div className="block transition-metals">
+                <div className="element-number">26</div>
+                <div className="element-symbol">Fe</div>
+                <div className="element-name">Iron</div>
+            </div>
+            <div className="block transition-metals">
+                <div className="element-number">27</div>
+                <div className="element-symbol">Co</div>
+                <div className="element-name">Cobalt</div>
+            </div>
+            <div className="block transition-metals">
+                <div className="element-number">28</div>
+                <div className="element-symbol">Ni</div>
+                <div className="element-name">Nickel</div>
+            </div>
+            <div className="block transition-metals">
+                <div className="element-number">29</div>
+                <div className="element-symbol">Cu</div>
+                <div className="element-name">Copper</div>
+            </div>
+            <div className="block transition-metals">
+                <div className="element-number">30</div>
+                <div className="element-symbol">Zn</div>
+                <div className="element-name">Zinc</div>
+            </div>
+            <div className="block post-transition-metals">
+                <div className="element-number">31</div>
+                <div className="element-symbol">Ga</div>
+                <div className="element-name">Gallium</div>
+            </div>
+            <div className="block metalloids">
+                <div className="element-number">32</div>
+                <div className="element-symbol">Ge</div>
+                <div className="element-name">Germanium</div>
+            </div>
+            <div className="block metalloids">
+                <div className="element-number">33</div>
+                <div className="element-symbol">As</div>
+                <div className="element-name">Arsenic</div>
+            </div>
+            <div className="block reactive-nonmetals">
+                <div className="element-number">34</div>
+                <div className="element-symbol">Se</div>
+                <div className="element-name">Selenium</div>
+            </div>
+            <div className="block reactive-nonmetals">
+                <div className="element-number">35</div>
+                <div className="element-symbol">Br</div>
+                <div className="element-name">Bromine</div>
+            </div>
+            <div className="block noble-gases">
+                <div className="element-number">36</div>
+                <div className="element-symbol">Kr</div>
+                <div className="element-name">Krypton</div>
+            </div>
 
-            <div className="block alkali-metals">K</div>
-            <div className="block alkaline-earth-metals">Ca</div>
-            <div className="block transition-metals">Sc</div>
-            <div className="block-empty"></div>
-            <div className="block transition-metals">Ti</div>
-            <div className="block transition-metals">V</div>
-            <div className="block transition-metals">Cr</div>
-            <div className="block transition-metals">Mn</div>
-            <div className="block transition-metals">Fe</div>
-            <div className="block transition-metals">Co</div>
-            <div className="block transition-metals">Ni</div>
-            <div className="block transition-metals">Cu</div>
-            <div className="block transition-metals">Zn</div>
-            <div className="block post-transition-metals">Ga</div>
-            <div className="block metalloids">Ge</div>
-            <div className="block metalloids">As</div>
-            <div className="block reactive-nonmetals">Se</div>
-            <div className="block reactive-nonmetals">Br</div>
-            <div className="block noble-gases">Kr</div>
-
-            <div className="block alkali-metals">Rb</div>
-            <div className="block alkaline-earth-metals">Sr</div>
-            <div className="block transition-metals">Y</div>
+            <div className="block alkali-metals">
+                <div className="element-number">37</div>
+                <div className="element-symbol">Rb</div>
+                <div className="element-name">Rubidium</div>
+            </div>
+            <div className="block alkaline-earth-metals">
+                <div className="element-number">38</div>
+                <div className="element-symbol">Sr</div>
+                <div className="element-name">Strontium</div>
+            </div>
+            <div className="block transition-metals">
+                <div className="element-number">39</div>
+                <div className="element-symbol">Y</div>
+                <div className="element-name">Yttrium</div>
+            </div>
             <div className="block-empty"></div>
             <div className="block transition-metals">Zr</div>
             <div className="block transition-metals">Nb</div>
@@ -101,9 +256,21 @@ function Table() {
             <div className="block reactive-nonmetals">I</div>
             <div className="block noble-gases">Xe</div>
 
-            <div className="block alkali-metals">Cs</div>
-            <div className="block alkaline-earth-metals">Ba</div>
-            <div className="block lanthanides">La</div>
+            <div className="block alkali-metals">
+                <div className="element-number">55</div>
+                <div className="element-symbol">Cs</div>
+                <div className="element-name">Caesium</div>
+            </div>
+            <div className="block alkaline-earth-metals">
+                <div className="element-number">56</div>
+                <div className="element-symbol">Ba</div>
+                <div className="element-name">Barium</div>
+            </div>
+            <div className="block lanthanides">
+                <div className="element-number">57</div>
+                <div className="element-symbol">La</div>
+                <div className="element-name">Lanthanum</div>
+            </div>
             <div className="block-empty"></div>
             <div className="block transition-metals">Hf</div>
             <div className="block transition-metals">Ta</div>
@@ -121,9 +288,21 @@ function Table() {
             <div className="block post-transition-metals">At</div>
             <div className="block noble-gases">Rn</div>
 
-            <div className="block alkali-metals">Fr</div>
-            <div className="block alkaline-earth-metals">Ra</div>
-            <div className="block actinides">Ac</div>
+            <div className="block alkali-metals">
+                <div className="element-number">87</div>
+                <div className="element-symbol">Fr</div>
+                <div className="element-name">Francium</div>
+            </div>
+            <div className="block alkaline-earth-metals">
+                <div className="element-number">88</div>
+                <div className="element-symbol">Ra</div>
+                <div className="element-name">Radium</div>
+            </div>
+            <div className="block actinides">
+                <div className="element-number">89</div>
+                <div className="element-symbol">Ac</div>
+                <div className="element-name">Actinium</div>
+            </div>
             <div className="block-empty"></div>
             <div className="block transition-metals">Rf</div>
             <div className="block transition-metals">Db</div>
